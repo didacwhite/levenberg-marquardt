3cefc6d816aa2c31919c3e15a8e171ae683ab5e4
didacwhite <63211429+didacwhite@users.noreply.github.com>
2023-01-25 16:08:05 +0100

    Delete implementacion directory

--- implementacion/rhofun.m
+++
@@ -1,6 +0,0 @@
-function rho = rhofun(funk,pk,lambdak,Dk,Jk,fun,xk)
-funplus = fun(xk+pk);
-
-rhonum=1-(norm(funplus)/norm(funk))^2;
-rhoden=(norm(Jk*pk')/norm(funk))^2+2*(sqrt(lambdak)*norm(Dk*pk')/norm(funk))^2;
-rho = rhonum/rhoden;
